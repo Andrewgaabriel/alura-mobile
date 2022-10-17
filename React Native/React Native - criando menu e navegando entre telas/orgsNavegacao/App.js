f7ae4875bb163c39c9c1c5e9119f610e86dc94f6
Andrewgaabriel <andrew.gabrielgomes@gmail.com>
2022-10-17 10:30:40 -0300

    fat: implementa navegaçãoe entre telas com stack & tab

--- React Native/React Native - criando menu e navegando entre telas/orgsNavegacao/App.js
+++ React Native/React Native - criando menu e navegando entre telas/orgsNavegacao/App.js
@@ -1,25 +1,13 @@
 import React from 'react';
 import { StatusBar, SafeAreaView } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import useProdutores from './src/hooks/useProdutores';
+import AppRotas from './src/rotas/AppRotas';
 
-import Home from './src/telas/Home';
-import Cesta from './src/telas/Cesta';
+
 
 export default function App() {
-  const produtores = useProdutores(false);
 
   return <SafeAreaView style={{ flex: 1 }}>
     <StatusBar />
-    <NavigationContainer>
-      <Home melhoresProdutores={false} />
-      {/* {produtores.length > 0 && 
-        <Cesta produtor={{
-          nome: produtores[0].nome, 
-          imagem: produtores[0].imagem
-        }}
-        {...produtores[0].cestas[0]} />
-      } */}
-    </NavigationContainer>
+    <AppRotas />
   </SafeAreaView>
 }
